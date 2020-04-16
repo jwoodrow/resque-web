29b073c8bc503ca392d880529f9600f492a0c18d
James Woodrow <james.woodrow@mooncard.co>
2020-04-16 15:18:57 +0200

    update application.js

--- app/assets/javascripts/resque_web/application.js
+++ app/assets/javascripts/resque_web/application.js
@@ -11,6 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery3
+//= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
