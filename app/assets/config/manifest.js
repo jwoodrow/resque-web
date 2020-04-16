45c2fddd79739b3daa550f3ad758fa2b55ce8d50
James Woodrow <james.woodrow@mooncard.co>
2020-04-16 15:11:30 +0200

    manifest.js

--- app/assets/config/manifest.js
+++
@@ -1,6 +0,0 @@
-/*
-  Below are Assets for the resque-web gem > website.com/resque_web
-*/
-//= link resque_web/application.js
-//= link resque_web/application.css
-//= link_directory ../images/resque_web .png
