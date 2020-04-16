ac8f1382747dedaa2579135961a41939c254bdf2
James Woodrow <james.woodrow@mooncard.co>
2020-04-16 13:50:23 +0200

    update to sprockets 4 with a manifest

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+/*
+  Below are Assets for the resque-web gem > website.com/resque_web
+*/
+//= link resque_web/application.js
+//= link resque_web/application.css
+//= link resque_web/idle.png
+//= link resque_web/lifebuoy.png
+//= link resque_web/poll.png
+//= link resque_web/rails.png
+//= link resque_web/working.png
