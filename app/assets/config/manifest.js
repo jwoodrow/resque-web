cab614ae395cd6dc8d7fa0c0787ee4eac2e6faef
James Woodrow <james.woodrow@mooncard.co>
2020-04-16 13:52:55 +0200

    update manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,8 +3,4 @@
 */
 //= link resque_web/application.js
 //= link resque_web/application.css
-//= link resque_web/idle.png
-//= link resque_web/lifebuoy.png
-//= link resque_web/poll.png
-//= link resque_web/rails.png
-//= link resque_web/working.png
+//= link_directory ../images/resque_web .png
