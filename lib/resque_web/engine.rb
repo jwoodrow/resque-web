7f2979571a5cb38fe2fcc79653842caa319a43c1
James Woodrow <james.woodrow@mooncard.co>
2020-04-16 15:16:16 +0200

    up

--- lib/resque_web/engine.rb
+++ lib/resque_web/engine.rb
@@ -8,6 +8,7 @@ module ResqueWeb
 
     initializer "resque_web.assets.precompile" do |app|
       app.config.assets.precompile += %w(resque_web/*.png)
+      app.config.assets.precompile += %w(resque_web/application.css resque_web/application.js)
     end
   end
   module Plugins
